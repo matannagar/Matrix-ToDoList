90158811a0d7ea2057c7a6329a85dd19fcc159ab
matannagar <73398186+matannagar@users.noreply.github.com>
2022-08-13 13:04:15 +0300

    fixed ability to add assignments

--- src/components/todo-form/todo-form.jsx
+++ src/components/todo-form/todo-form.jsx
@@ -2,13 +2,17 @@ import * as React from "react";
 import "./todo-form.scss";
 import { useContext } from "react";
 import { TodosContext } from "../../todo-context";
+import { v4 as uuidv4 } from "uuid";
 
 
 export const TodoForm = () => {
   const [todos, setTodos] = useContext(TodosContext);
   const [task, setTask] = React.useState("");
 
-  const handleAddTodo = () => { };
+  const handleAddTodo = (event) => {
+
+    setTodos([...todos, { id: uuidv4(), label: task, checked: false }])
+  };
 
   const handleKeyUp = (e) => {
     if (e.keyCode === 13) {
