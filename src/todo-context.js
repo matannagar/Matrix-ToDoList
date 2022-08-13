b06e076e3db3294b238a8f45d0e5a6b81fa1781a
matannagar <73398186+matannagar@users.noreply.github.com>
2022-08-13 19:01:26 +0300

    ready to heroku

--- src/todo-context.js
+++ src/todo-context.js
@@ -5,37 +5,6 @@ export const TodosContext = createContext({});
 export const TodosProvider = props => {
     const initialState = JSON.parse(localStorage.getItem("todos")) || [];
     const [todos, setTodos] = useState(initialState);
-    // {
-    //     id: 0,
-    //     label: "Fix an ability to display all tasks",
-    //     checked: false,
-    // },
-    // {
-    //     id: 1,
-    //     label: "Fix a layout, checkboxes should be listed in a column",
-    //     checked: false,
-    // },
-    // {
-    //     id: 2,
-    //     label: "Fix an ability to add a new task",
-    //     checked: false,
-    // },
-    // {
-    //     id: 3,
-    //     label: "Fix an ability to toggle a task",
-    //     checked: false,
-    // },
-    // {
-    //     id: 4,
-    //     label: "Fix an ability to delete a task",
-    //     checked: false,
-    // },
-    // {
-    //     id: 5,
-    //     label: "Fix an ability to count completed tasks",
-    //     checked: false,
-    // },
-    // ]);
 
     const [task, setTask] = useState('');
     useEffect(() => {
