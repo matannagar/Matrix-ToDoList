5807db9ef5264a560d605d6ff7943e783dbbbc91
matannagar <73398186+matannagar@users.noreply.github.com>
2022-08-13 11:39:56 +0300

    first commit

---
+++ src/components/todo-form/todo-form.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import "./todo-form.scss";
+
+export const TodoForm = () => {
+  const [task, setTask] = React.useState("");
+
+  const handleAddTodo = () => {};
+
+  const handleKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="todo-form">
+      <input
+        placeholder="Enter new task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
+      <button type="button" onClick={handleAddTodo}>
+        Add task
+      </button>
+    </div>
+  );
+};
---
+++ src/components/todo-results/todo-results.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./todo-results.scss";
+
+export const TodoResults = () => {
+  const calculateChecked = () => {};
+
+  return (
+    <div className="todo-results">
+      Done:
+      {calculateChecked()}
+    </div>
+  );
+};
