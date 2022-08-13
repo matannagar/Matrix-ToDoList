e294b3dc96129341a956657c9d96e2375b9bae80
matannagar <73398186+matannagar@users.noreply.github.com>
2022-08-13 18:29:14 +0300

    added localStorage caching

--- src/todo-context.js
+++ src/todo-context.js
@@ -1,42 +1,46 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
 export const TodosContext = createContext({});
 
 export const TodosProvider = props => {
-    const [todos, setTodos] = useState([
-        {
-            id: 0,
-            label: "Fix an ability to display all tasks",
-            checked: false,
-        },
-        {
-            id: 1,
-            label: "Fix a layout, checkboxes should be listed in a column",
-            checked: false,
-        },
-        {
-            id: 2,
-            label: "Fix an ability to add a new task",
-            checked: false,
-        },
-        {
-            id: 3,
-            label: "Fix an ability to toggle a task",
-            checked: false,
-        },
-        {
-            id: 4,
-            label: "Fix an ability to delete a task",
-            checked: false,
-        },
-        {
-            id: 5,
-            label: "Fix an ability to count completed tasks",
-            checked: false,
-        },
-    ]);
+    const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+    const [todos, setTodos] = useState(initialState);
+    // {
+    //     id: 0,
+    //     label: "Fix an ability to display all tasks",
+    //     checked: false,
+    // },
+    // {
+    //     id: 1,
+    //     label: "Fix a layout, checkboxes should be listed in a column",
+    //     checked: false,
+    // },
+    // {
+    //     id: 2,
+    //     label: "Fix an ability to add a new task",
+    //     checked: false,
+    // },
+    // {
+    //     id: 3,
+    //     label: "Fix an ability to toggle a task",
+    //     checked: false,
+    // },
+    // {
+    //     id: 4,
+    //     label: "Fix an ability to delete a task",
+    //     checked: false,
+    // },
+    // {
+    //     id: 5,
+    //     label: "Fix an ability to count completed tasks",
+    //     checked: false,
+    // },
+    // ]);
 
     const [task, setTask] = useState('');
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
     return (
         <TodosContext.Provider value={[todos, setTodos, task, setTask]}>
